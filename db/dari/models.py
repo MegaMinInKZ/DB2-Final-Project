215b0f00391fdb3e64bd0a4b3cf537602154155b
MegaMinInKZ <200103287@stu.sdu.edu.kz>
2022-04-18 11:18:50 +0600

    other tables are added

--- db/dari/models.py
+++ db/dari/models.py
@@ -1,4 +1,8 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.core.validators import MaxValueValidator, MinValueValidator
+
+
 
 # Create your models here.
 class Category(models.Model):
@@ -12,3 +16,38 @@ class Product(models.Model):
     amount = models.IntegerField()
     Image = models.URLField(max_length = 200)
     category = models.ForeignKey(Category, on_delete=models.CASCADE)
+
+class Rating(models.Model):
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    date = models.DateField(auto_now_add=True)
+    value = models.IntegerField(validators=[MinValueValidator(1), MaxValueValidator(5)])
+
+class Price_History(models.Model):
+    price = models.IntegerField()
+    date = models.DateField(auto_now_add=True)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+
+class Comment(models.Model):
+    comment_text = models.TextField()
+    date = models.DateField(auto_now_add=True)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+class Cart(models.Model):
+    date = models.DateField(auto_now_add=True)
+    amount = models.IntegerField()
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+
+class Purchase(models.Model):
+    date = models.DateField(auto_now_add=True)
+    amount = models.IntegerField()
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+
+class Feedback(models.Model):
+    description = models.TextField()
+    date = models.DateField()
+    purchase = models.ForeignKey(Purchase, on_delete=models.CASCADE)
+
